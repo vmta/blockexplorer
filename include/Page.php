1def3d22c293e21930ccf49fcbd41e29d2829950
Igor Gunia <vmta@yahoo.com>
2018-01-17 12:58:34 +0200

    Update Block review section

--- include/Page.php
+++ include/Page.php
@@ -277,43 +277,49 @@ class Page {
 
         case "blockhash":
 
-          $this->html_content_string .= "<h2><i class='fa fa-cube fa-fw' aria-hidden='true'></i> Block <small id='block.hash' style='word-break: break-all;'>" . $reqval . "</small></h2>" .
-            "<div class='row'>" .
-              "<div class='col-md-6 stats'>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Block index in the chain, counting from zero (i.e. genesis block).'><i class='fa fa-question-circle'></i></span> Height: <span id='block_height'><span id='block.height'>" . $this->block->getblockheight($reqval) . "</span></span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Block timestamp displayed as UTC. The timestamp correctness it up to miner, who mined the block.'><i class='fa fa-question-circle'></i></span> Timestamp: <span id='block.timestamp'>" . gmdate("M d, Y H:i:s", $this->block->getblocktime($reqval)) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='How difficult it is to find a solution for the block. More specifically, it`s mathematical expectation for number of hashes someone needs to calculate in order to find a correct nonce value solving the block.'><i class='fa fa-question-circle'></i></span> Difficulty: <span id='block.difficulty'>" . $this->block->getblockdifficulty($reqval) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Number of transactions in the block, including coinbase transaction (which transfers block reward to the miner).'><i class='fa fa-question-circle'></i></span> Transactions: <span id='block.transactions'>" . $this->block->getblocktransactionscount($reqval) . "</span></a></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative amount of coins issued by all the blocks in blockchain from the genesis and up to this block.'><i class='fa fa-question-circle'></i></span> Total coins in the network: <span id='block.totalCoins'>" . $this->block->getsupply() . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative number of transactions in the blockchain, from the genesis block and up to this block.'><i class='fa fa-question-circle'></i></span> Total transactions in the network: <span id='block.totalTransactions'>" . $this->block->gettransactioncount() . "</span></div>" .
-              "</div>" .
+          $this->html_content_string .= "<div id='content>" .
+            "<div class='container'>" .
+              "<div id='page'>" .
+                "<h2><i class='fa fa-cube fa-fw' aria-hidden='true'></i> Block <small id='block.hash' style='word-break: break-all;'>" . $reqval . "</small></h2>" .
+                "<div class='row'>" .
+                  "<div class='col-md-6 stats'>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Block index in the chain, counting from zero (i.e. genesis block).'><i class='fa fa-question-circle'></i></span> Height: <span id='block_height'><span id='block.height'>" . $this->block->getblockheight($reqval) . "</span></span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Block timestamp displayed as UTC. The timestamp correctness it up to miner, who mined the block.'><i class='fa fa-question-circle'></i></span> Timestamp: <span id='block.timestamp'>" . gmdate("M d, Y H:i:s", $this->block->getblocktime($reqval)) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='How difficult it is to find a solution for the block. More specifically, it`s mathematical expectation for number of hashes someone needs to calculate in order to find a correct nonce value solving the block.'><i class='fa fa-question-circle'></i></span> Difficulty: <span id='block.difficulty'>" . $this->block->getblockdifficulty($reqval) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Number of transactions in the block, including coinbase transaction (which transfers block reward to the miner).'><i class='fa fa-question-circle'></i></span> Transactions: <span id='block.transactions'>" . $this->block->getblocktransactionscount($reqval) . "</span></a></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative amount of coins issued by all the blocks in blockchain from the genesis and up to this block.'><i class='fa fa-question-circle'></i></span> Total coins in the network: <span id='block.totalCoins'>" . $this->block->getsupply() . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative number of transactions in the blockchain, from the genesis block and up to this block.'><i class='fa fa-question-circle'></i></span> Total transactions in the network: <span id='block.totalTransactions'>" . $this->block->gettransactioncount() . "</span></div>" .
+                  "</div>" .
 
-              "<div class='col-md-6 stats'>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative size of all transactions in the block, including coinbase. In case it's exceeding 'effective txs median' the reward penalty occurs and therefore miner receives less reward.'><i class='fa fa-question-circle'></i></span> Total transactions size, bytes: <span id='block.transactionsSize'>" . $this->block->getblocktransactionssize($reqval) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Size of the whole block, i.e. block header plus all transactions.'><i class='fa fa-question-circle'></i></span> Total block size, bytes: <span id='block.blockSize'>" . $this->block->getblocksize($reqval) .  "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Sum of values for all transactions in the block.'><i class='fa fa-question-circle'></i></span> Transactions amount: <span id='block.totalAmount'>" . $this->block->getblockamount($reqval) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Sum of fees for all transactions in the block.'><i class='fa fa-question-circle'></i></span> Transactions fee: <span id='block.transactionsFee'>" . $this->block->getblockfees($reqval) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Base value for calculating the block reward. Does not depend on how many transactions are included into the block. Also, this is how many coins the miner would receive if the block contains only coinbase transaction.'><i class='fa fa-question-circle'></i></span> Base reward: <span id='block.baseReward'>" . $this->block->getbasereward($reqval) . "</span></div>" .
-                "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Actual amount of coins the miner received for finding the block. &lt;reward&gt; = &lt;base reward&gt; × (1 − &lt;penalty&gt;) + &lt;transactions fee&gt;'><i class='fa fa-question-circle'></i></span> Reward: <span id='block.reward'>" . $this->block->getreward($reqval, 'hash') . "</span></div>" .
+                  "<div class='col-md-6 stats'>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Cumulative size of all transactions in the block, including coinbase. In case it's exceeding 'effective txs median' the reward penalty occurs and therefore miner receives less reward.'><i class='fa fa-question-circle'></i></span> Total transactions size, bytes: <span id='block.transactionsSize'>" . $this->block->getblocktransactionssize($reqval) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Size of the whole block, i.e. block header plus all transactions.'><i class='fa fa-question-circle'></i></span> Total block size, bytes: <span id='block.blockSize'>" . $this->block->getblocksize($reqval) .  "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Sum of values for all transactions in the block.'><i class='fa fa-question-circle'></i></span> Transactions amount: <span id='block.totalAmount'>" . $this->block->getblockamount($reqval) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Sum of fees for all transactions in the block.'><i class='fa fa-question-circle'></i></span> Transactions fee: <span id='block.transactionsFee'>" . $this->block->getblockfees($reqval) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Base value for calculating the block reward. Does not depend on how many transactions are included into the block. Also, this is how many coins the miner would receive if the block contains only coinbase transaction.'><i class='fa fa-question-circle'></i></span> Base reward: <span id='block.baseReward'>" . $this->block->getbasereward($reqval) . "</span></div>" .
+                    "<div><span data-toggle='tooltip' data-placement='right' data-original-title='Actual amount of coins the miner received for finding the block. &lt;reward&gt; = &lt;base reward&gt; × (1 − &lt;penalty&gt;) + &lt;transactions fee&gt;'><i class='fa fa-question-circle'></i></span> Reward: <span id='block.reward'>" . $this->block->getreward($reqval, 'hash') . "</span></div>" .
+                  "</div>" .
+                "</div>" .
+
+                "<h3 class='transactions'><i class='fa fa-exchange fa-fw' aria-hidden='true'></i> Transactions</h3>" .
+                "<div class='table-responsive'>" .
+                  "<table class='table table-hover'>" .
+                  "<thead>" .
+                  "<tr>" .
+                    "<th><i class='fa fa-paw'></i> Hash</th>" .
+                    "<th><i class='fa fa-percent'></i> Fee</th>" .
+                    "<th><i class='fa fa-money'></i> Total Amount</th>" .
+                    "<th><i class='fa fa-arrows'></i> Size</th>" .
+                  "</tr>" .
+                  "</thead>" .
+                  "<tbody id='transactions_rows'>" .
+                    $this->block->getblocktransactionshtml($reqval) .
+                  "</tbody>" .
+                  "</table>" .
+                "</div>" .
               "</div>" .
             "</div>" .
-
-            "<h3 class='transactions'><i class='fa fa-exchange fa-fw' aria-hidden='true'></i> Transactions</h3>" .
-            "<div class='table-responsive'>" .
-              "<table class='table table-hover'>" .
-              "<thead>" .
-              "<tr>" .
-                "<th><i class='fa fa-paw'></i> Hash</th>" .
-                "<th><i class='fa fa-percent'></i> Fee</th>" .
-                "<th><i class='fa fa-money'></i> Total Amount</th>" .
-                "<th><i class='fa fa-arrows'></i> Size</th>" .
-              "</tr>" .
-              "</thead>" .
-              "<tbody id='transactions_rows'>" .
-
-              "</tbody>" .
-              "</table>" .
-            "</div>";
+          "</div>";
 
           break;
 
