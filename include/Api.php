4d9b87ee3f51fbb6b612723217ff8b08f5d039b1
Igor Gunia <vmta@yahoo.com>
2018-01-25 10:04:43 +0200

    Update dumpwallet function

--- include/Api.php
+++ include/Api.php
@@ -2347,14 +2347,25 @@ class Api {
 
 
   /* 65
+   * dumpwallet "filename"
    *
-   * Returns
+   * Dumps all wallet keys in a human-readable format to a server-side file. This does not allow overwriting existing files.
+   * Imported scripts are included in the dumpfile, but corresponding BIP173 addresses, etc. may not be added automatically by importwallet.
+   * Note that if your wallet contains keys which are not derived from your HD seed (e.g. imported keys), these are not covered by
+   * only backing up the seed itself, and must be backed up too (e.g. ensure you back up the whole dumpfile).
+   *
+   * Arguments:
+   * 1. "filename"  (string, required) The filename with path (either absolute or relative to umkoind)
+   *
+   * Result:
+   * "filename" :   (string) The filename with full absolute path
    *
    */
   public function dumpwallet($filename) {
 
     $args = $this->args;
     $args["method"] = __FUNCTION__;
+    $args["params"] = ["$filename"];
 
     $res = $this->call($args);
     if ($res)
